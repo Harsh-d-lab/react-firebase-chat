965510921e39ffb04036d9624137365a6ba6bf90
Harsh Dhama <dhamaharsh9@gmail.com>
2024-09-10 20:44:37 +0530

    chatlist updated

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import "./Login.css"
+
+const Login = () => {
+    return (
+        <div className="login">
+        <div className="item"></div>
+        <div className="separator"></div>
+        </div>
+    )
+}
+
+export default Login
